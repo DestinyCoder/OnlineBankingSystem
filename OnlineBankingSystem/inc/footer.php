4db03a47c1404393924cf02a1fb2d52623563e66
jitendra1998 <rajpurohitjitendra81@gmail.com>
2017-08-29 01:16:27 +0530

    added names

--- OnlineBankingSystem/inc/footer.php
+++ OnlineBankingSystem/inc/footer.php
@@ -1,4 +1,5 @@
 <footer>
-	 <hr>
-	 <p style="text-align: right;">Site design by ..... Copyright © <?php echo date("Y"); ?> World Bank</p>
+	<hr style="margin: 0;border-top: 0">
+	<p style="text-align: right;color: white">Copyright © <?php echo date("Y"); ?> World Bank</p>
+	<p style="font-size: 22px; font-family:'Berkshire Swash',cursive; color:orange;"><marquee direction="left" onMouseOver="this.stop()" onMouseOut="this.start()">Made By: Jitendra Rajpurohit, Pawan Yadav, Niraj Alte, Varun Panday</marquee> </p>
  </footer>
