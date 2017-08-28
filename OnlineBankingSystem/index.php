d6cbb99d3b3f8651e082e7c744bf6c1f828af84e
jitendra1998 <rajpurohitjitendra81@gmail.com>
2017-08-29 01:24:46 +0530

    completed index.php

--- OnlineBankingSystem/index.php
+++ OnlineBankingSystem/index.php
@@ -16,7 +16,7 @@
 		<header>
 			<div class="row">
 				<div class="col-sm-3 col-md-3 col-lg-3 hidden-xs">
-					<a href="index.html"><img  id="logo" src="images/logo.png" alt="The World Bank Logo" class="img-responsive img-rounded"></a>
+					<a href="index.php"><img  id="logo" src="images/logo.png" alt="The World Bank Logo" class="img-responsive img-rounded"></a>
 				</div>
 			<!--<div class="col-sm-9 col-md-9 col-lg-9">
 					<h1 style="float: right;color: lightblue">World Bank</h1>
@@ -24,7 +24,7 @@
 			</div>
 		</header>
 		<div class="row">
-			<div class="col-sm-12 col-md-12 col-lg-12 col-comn">
+			<div class="col-sm-12 col-md-12 col-lg-12">
 				<nav class="navbar navbar-default">
 					<div class="navbar-header hidden-sm hidden-md hidden-lg">
 						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#my_navbar">
@@ -37,12 +37,12 @@
 					</div>
 					<div class="collapse navbar-collapse" id="my_navbar" style="height: 0px">
 						<ul class="nav navbar-nav" id="main_nav">
-							<li><a href="index.html" role="button">Home</a></li>
-							<li><a href="login.html" role="button">Login</a></li>
-							<li><a href="branch.html" role="button">Branch</a></li>
-							<li><a href="help.html" role="button">Help</a></li>
-							<li><a href="contact.html" role="button">Contact Us</a></li>
-							<li><a href="apply.html" role="button">Apply</a></li>
+							<li><a href="index.php" role="button">Home</a></li>
+							<li><a href="login.php" role="button">Login</a></li>
+							<li><a href="branch.php" role="button">Branch</a></li>
+							<li><a href="help.php" role="button">Help</a></li>
+							<li><a href="contact.php" role="button">Contact Us</a></li>
+							<li><a href="apply.php" role="button">Apply</a></li>
 						</ul>
 					</div>
 				</nav>
@@ -67,72 +67,74 @@
 					</div>
 				</div>
 			</div>
-			<div class="row" id="feature_area">
+			<div class="row features_area">
 				<div class="col-sm-12 col-md-10 col-lg-10 col-lg-offset-1 col-md-offset-1 col-comn">
 					<div class="row">
 						<div class="col-sm-3 col-md-3 col-lg-3 loan_area">
 							<div class="loan_features">
-								<span id="features_a">&nbsp;</span>
+								<span id="features_a"></span>
 								<h3>Home Loan</h3>
 								<p>@8.35%<sup>*</sup></p>
-								<a href="https://onlineapply.sbi.co.in/personal-banking/home-loan">Apply Now <span class="apply_arrow">&nbsp;</span></a>
+								<a href="">Apply Now <span class="apply_arrow"></span></a>
 							</div>
-							<span class="loan_separtor hidden-xs">&nbsp;</span>
+							<span class="loan_separtor hidden-xs"></span>
 						</div>
 						<div class="col-sm-3 col-md-3 col-lg-3 loan_area">
 							<div class="loan_features">
-								<span id="features_b">&nbsp;</span>
+								<span id="features_b"></span>
 								<h3>Car Loan</h3>
 								<p>@8.75%<sup>*</sup></p>
-								<a href="https://onlineapply.sbi.co.in/personal-banking/auto-loan?se=Main&amp;cp=SBICOIN&amp;ag=General">Apply Now <span class="apply_arrow">&nbsp;</span></a>
+								<a href="">Apply Now <span class="apply_arrow"></span></a>
 							</div>
-							<span class="loan_separtor hidden-xs">&nbsp;</span>
+							<span class="loan_separtor hidden-xs"></span>
 						</div>
 						<div class="col-sm-3 col-md-3 col-lg-3 loan_area">
 							<div class="loan_features">
-								<span id="features_e">&nbsp;</span>
+								<span id="features_c"></span>
 								<h3>Savings Bank </h3>
 								<p>Online Account Opening</p>
-								<a href="/portal/web/personal-banking/savings-bank-account">Apply Now <span class="apply_arrow">&nbsp;</span></a>
+								<a href="">Apply Now <span class="apply_arrow"></span></a>
 							</div>
-							<span class="loan_separtor hidden-xs">&nbsp;</span>
+							<span class="loan_separtor hidden-xs"></span>
 						</div>
 						<div class="col-sm-3 col-md-3 col-lg-3 loan_area">
 							<div class="loan_features">
-								<span id="features_d">&nbsp;</span>
+								<span id="features_d"></span>
 								<h3>Personal Loan</h3>
 								<p>SBI Loan Scheme</p>
-								<a href="https://onlineapply.sbi.co.in/personal-banking/personal-loan?se=SBI.CO.IN&amp;cp=MainPage&amp;ag=General">Apply Now <span class="apply_arrow">&nbsp;</span></a>
+								<a href="">Apply Now <span class="apply_arrow"></span></a>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-			<div class="home">
-            	<div class="begin"> <h2><a href="Register.php">Register For NetBanking Now!!</a></h2> <br/>Now monitor, transact and control your bank account online through our net banking service. 
-            	You can do multiple things from the comforts of your home or office with our Internet Banking - a one stop solution
-             	for all your banking needs.You can now get all your accounts details, submit requests and undertake a wide range of transactions online. Our E-Banking service 
-             	makes banking a lot more easy and effective.
-             	</div>
-             	<br/>
-             	<br/>
-             	<span id="feat" ><b><u>Features</u></b></span><br/><br/>
-             	<ul style="padding-left:2em;list-style-image:url('images/next_blue.png');align:middle;"><li><span class="subhead">Account Details</span><br/><br/>View your bank account details, account balance, download statements and more. Also view your Demat, 
-                Loan & Credit Card Account Details too all in one place.</li><br/>
-                <li><span class="subhead">Fund Transfer</span><br/><br/>Transfer fund to your own accounts,Other Gotham Bank accounts seamlessly.</li><br/>
-                <li><span class="subhead">Request Services</span><br/><br/>Give a request for Cheque book,Demand Draft,Stop Cheque Payment,Debit Card Loyalty Point Redemption etc.</li><br/>
-                <li><span class="subhead">Investment Services</span><br/><br/>View your complete Portfolio with the bank, Create Fixed Deposit, Apply for IPO </li><br/>
-                <li><span class="subhead">Value Added Services</span><br/><br/>Pay Utility bills for more than 160 billers, Recharge Mobile, Create Virtual Cards, Pay any Visa Credit Card bills,
-				Register for estatement and sms banking</li></ul><br/>
-				<div class="begin">Register now for Gotham Bank's internet banking service to get started and avail for you multiple utility services, all in a matter of a click. Getting started with our internet banking is real easy. 
-				All you need to do is follow a few simple steps and you are good to go. <a href="Register.php">Click here</a> for our registration process
-				<p>We at the B.O.G.C follow best-in-class online security practices in order to safeguard your information while you are banking online. We are constantly at task for preventing fraud and thereby making all your net banking transactions completely safe.
-         		</div>
+			<div class="row features_area">
+				<div class="register">
+					<h2><a href="Register.php">Register For NetBanking Now!!</a></h2> 
+					<p>
+						Now monitor, transact and control your bank account online through our net banking service. 
+            			You can do multiple things from the comforts of your home or office with our Internet Banking - a one stop solution
+             			for all your banking needs.You can now get all your accounts details, submit requests and undertake a wide range of transactions online. Our E-Banking service makes banking a lot more easy and effective.
+            		</p>
+            		<p id="features">Features</p>
+            		<ul class="feature_list">
+            			<li><span>Account Details</span><p>View your bank account details, account balance, download statements and more. Also view your Demat, 
+                		Loan & Credit Card Account Details too all in one place.</p></li>
+
+                		<li><span>Fund Transfer</span><p>Transfer fund to your own accounts,Other Gotham Bank accounts seamlessly.</p></li>
+
+                		<li><span>Request Services</span><p>Give a request for Cheque book,Demand Draft,Stop Cheque Payment,Debit Card Loyalty Point Redemption etc.</p></li>
+
+                		<li><span>Investment Services</span><p>View your complete Portfolio with the bank, Create Fixed Deposit, Apply for IPO.</p></li>
+
+                		<li><span>Value Added Services</span><p>Pay Utility bills for more than 160 billers, Recharge Mobile, Create Virtual Cards, Pay any Visa Credit Card bills,Register for estatement and sms banking</p></li>
+            		</ul>
+            		<p>Register now for World Bank's internet banking service to get started and avail for you multiple utility services, all in a matter of a click. Getting started with our internet banking is real easy. All you need to do is follow a few simple steps and you are good to go. <a href="Register.php">Click here</a> for our registration process.</p>
+					<p>We at the World Bank follow best-in-class online security practices in order to safeguard your information while you are banking online. We are constantly at task for preventing fraud and thereby making all your net banking transactions completely safe.</p>
+            	</div>
          	</div>
 		</section>
-		<div>
 		<?php include 'inc/footer.php'; ?>
-		</div>
 	</div>
 	<script type="text/javascript" src=""></script>
 </body>
