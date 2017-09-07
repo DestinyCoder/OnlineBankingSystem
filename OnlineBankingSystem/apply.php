72001e3b666d26b14eb44a759bfd2d9a66bd3e0a
pawan1107 <pawan.yadav1107@gmail.com>
2017-09-07 10:29:36 +0530

    added login and register page

---
+++ OnlineBankingSystem/apply.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>The World Bank</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<link rel="shortcut icon" href="images/icon.png">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/button_style.css">
+</head>
+<body>
+	<div class="container" id="wrapper">
+		<div id="white"></div>
+		<header>
+			<div class="row">
+				<div class="col-sm-3 col-md-3 col-lg-3 hidden-xs">
+					<a href="index.php"><img  id="logo" src="images/logo.png" alt="The World Bank Logo" class="img-responsive img-rounded"></a>
+				</div>
+			<!--<div class="col-sm-9 col-md-9 col-lg-9">
+					<h1 style="float: right;color: lightblue">World Bank</h1>
+				</div>-->
+			</div>
+		</header>
+		<div class="row">
+			<div class="col-sm-12 col-md-12 col-lg-12">
+				<nav class="navbar navbar-default">
+					<div class="navbar-header hidden-sm hidden-md hidden-lg">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#my_navbar">
+							<span class="sr-only">Toggle Navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand" href="index.html"></a><img id="logo" class="img-responsive img-rounded" src="images/logo.png" alt="The World Bank Logo">
+					</div>
+					<div class="collapse navbar-collapse" id="my_navbar" style="height: 0px">
+						<ul class="nav navbar-nav" id="main_nav">
+							<li><a href="index.php" role="button">Home</a></li>
+							<li><a href="login.php" role="button">Login</a></li>
+							<li><a href="branch.php" role="button">Branch</a></li>
+							<li><a href="help.php" role="button">Help</a></li>
+							<li><a href="contact.php" role="button">Contact Us</a></li>
+							<li><a href="apply.php" role="button">Apply</a></li>
+						</ul>
+					</div>
+				</nav>
+			</div>
+		</div>
+ 		<div class="features_area row" >	
+			<div class="row_register" >
+				<h1><center>Register</center></h1>
+				<form  name = "myForm" class="register_form" onsubmit=" " method="post">
+				<table> 
+					<tr>
+						<td><label><b>Acount No</b></label>
+						</td><td>:-</td>
+						<td><input class="register" type="number" placeholder="Enter Account No." name="account" required> <br><br></td>
+					</tr>
+					<tr>
+		 				<td><label><b>CIF No</b></label></td>
+		 				<td>:-</td>
+						<td><input class="register" type="number" placeholder="Enter CIF No." name="cif" required> <br><br></td>
+					</tr>
+					<tr> 
+						<td><label><b>Branch code</b></label></td>
+						<td>:-</td>
+						<td><input class="register" type="number" placeholder="Enter Branch code" name="account" required></center><br><br></td>
+					</tr>
+					<tr>
+						<td> <label><b>Email</b></label></td>
+						<td>:-</td>
+						<td><input class="register" type="Email" placeholder="Enter Email" name="email" required> <br><br></td>
+					</tr>
+					<tr> 
+						<td><label><b>Registered mobile No</b></label></td>
+						<td>:-</td>
+						<td><input class="register" type="number" placeholder="Enter mobile no" name="mobile" max ="10" required> <br><br></td>
+					</tr>
+    			   </table>
+    			   <div class="button_register">
+    			   		<button class="button1" id="register_button1" type="submit" name="submit"  ><span>Submit</span></button>
+      					<button class="button1" id="register_button2" type="reset"><span>Reset</span></button>     
+					</div>
+				</form> 
+			</div>	 
+		</div>
+			 
+		<?php include 'inc/footer.php'; ?>
+	</div>
+	<script type="text/javascript" src=""></script>
+</body>
+</html>
---
+++ OnlineBankingSystem/login.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>The World Bank</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<link rel="shortcut icon" href="images/icon.png">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/button_style.css">
+</head>
+<body>
+	<div class="container" id="wrapper">
+		<div id="white"></div>
+		<header>
+			<div class="row">
+				<div class="col-sm-3 col-md-3 col-lg-3 hidden-xs">
+					<a href="index.php"><img  id="logo" src="images/logo.png" alt="The World Bank Logo" class="img-responsive img-rounded"></a>
+				</div>
+			<!--<div class="col-sm-9 col-md-9 col-lg-9">
+					<h1 style="float: right;color: lightblue">World Bank</h1>
+				</div>-->
+			</div>
+		</header>
+		<div class="row">
+			<div class="col-sm-12 col-md-12 col-lg-12">
+				<nav class="navbar navbar-default">
+					<div class="navbar-header hidden-sm hidden-md hidden-lg">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#my_navbar">
+							<span class="sr-only">Toggle Navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand" href="index.html"></a><img id="logo" class="img-responsive img-rounded" src="images/logo.png" alt="The World Bank Logo">
+					</div>
+					<div class="collapse navbar-collapse" id="my_navbar" style="height: 0px">
+						<ul class="nav navbar-nav" id="main_nav">
+							<li><a href="index.php" role="button">Home</a></li>
+							<li><a href="login.php" role="button">Login</a></li>
+							<li><a href="branch.php" role="button">Branch</a></li>
+							<li><a href="help.php" role="button">Help</a></li>
+							<li><a href="contact.php" role="button">Contact Us</a></li>
+							<li><a href="apply.php" role="button">Apply</a></li>
+						</ul>
+					</div>
+				</nav>
+			</div>
+		</div>
+ 		<div class="features_area row" id="middle_box" style="padding: 0;">	
+				<div class="row_login">
+				<center><h2 id="login_span">Login</h2></center>
+				
+					<form action="" style="margin-top: 20px;">
+						<label>Username:</label><br>
+ 						<input type="text" class="login" name="username" required ><br>
+  						<label>Password:</label><br>
+  						<input type="Password" class="login" name="password" required ><br><br>
+ 						<button class="button1" id="login_button1" type="submit"><span>Login</span></button>
+ 				 		<button class="button1" id="login_button2" type="reset"><span>Reset</span></button>
+					</form>
+				</div> 
+				<div class="mid_login">
+					<br><br><br><br><br>
+					<a class="extra" href="">Forgot Password</a><br><br><br>
+					<a class="extra" href="">Forgot Username</a><br><br><br>
+					<a class="extra" href="apply.php">New User?/Register</a><br><br>
+				</div>
+				 
+					<img id="login_page_img" src="images/indeximg.jpg">
+				 
+			</div>
+			<section id="content_area_section">
+			<div class="row features_area">
+				<div class="col-sm-12 col-md-10 col-lg-10 col-lg-offset-1 col-md-offset-1 col-comn">
+					<div class="row">
+						<div class="col-sm-3 col-md-3 col-lg-3 loan_area">
+							<div class="loan_features">
+								<span id="features_a"></span>
+								<h3>Home Loan</h3>
+								<p>@8.35%<sup>*</sup></p>
+								<a href="">Apply Now <span class="apply_arrow"></span></a>
+							</div>
+							<span class="loan_separtor hidden-xs"></span>
+						</div>
+						<div class="col-sm-3 col-md-3 col-lg-3 loan_area">
+							<div class="loan_features">
+								<span id="features_b"></span>
+								<h3>Car Loan</h3>
+								<p>@8.75%<sup>*</sup></p>
+								<a href="">Apply Now <span class="apply_arrow"></span></a>
+							</div>
+							<span class="loan_separtor hidden-xs"></span>
+						</div>
+						<div class="col-sm-3 col-md-3 col-lg-3 loan_area">
+							<div class="loan_features">
+								<span id="features_c"></span>
+								<h3>Savings Bank </h3>
+								<p>Online Account Opening</p>
+								<a href="">Apply Now <span class="apply_arrow"></span></a>
+							</div>
+							<span class="loan_separtor hidden-xs"></span>
+						</div>
+						<div class="col-sm-3 col-md-3 col-lg-3 loan_area">
+							<div class="loan_features">
+								<span id="features_d"></span>
+								<h3>Personal Loan</h3>
+								<p>SBI Loan Scheme</p>
+								<a href="">Apply Now <span class="apply_arrow"></span></a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+ 
+		</section>
+		<?php include 'inc/footer.php'; ?>
+	</div>
+	<script type="text/javascript" src=""></script>
+</body>
+</html>
