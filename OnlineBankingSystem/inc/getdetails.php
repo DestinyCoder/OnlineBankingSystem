a20c7ebf10ab9abc2c80f1f71152a07d3434dcb1
pawan1107 <pawan.yadav1107@gmail.com>
2017-09-10 18:30:31 +0530

    updated/remove error from getdetails.php and get_trans_dt.php

--- OnlineBankingSystem/inc/getdetails.php
+++ OnlineBankingSystem/inc/getdetails.php
@@ -1,14 +1,14 @@
 <?php
  include 'connection.php';
- include 'getdetails.php';
+ //necesarry include to get username
  // getting customer details  
- $cust_result = mysqli_query("SELECT * FROM customer WHERE username = '".mysql_real_escape_string($username). "'");
+ $cust_result = mysqli_query($conn,"SELECT * FROM customer WHERE username = '".mysqli_real_escape_string($conn,$username). "'");
  $row_cust = mysqli_fetch_assoc($cust_result);
  /*
  we can ascess name by $row_cust["name"] and like this every column 
  */
  //getting account details 
- $acc_result = mysqli_query("SELECT * FROM account WHERE customer_id = '".mysql_real_escape_string($row_cust["customer_id"]). "'");
+ $acc_result = mysqli_query($conn,"SELECT * FROM account WHERE customer_id = '".mysqli_real_escape_string($conn,$row_cust["customer_id"]). "'");
  $row_acc = mysqli_fetch_assoc($acc_result);
  /*
  we can ascess balance by $row_acc["balance"] and like this every column 
