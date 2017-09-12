7b765dc097664ef4738599411730a1b0a6e0a2d6
pawan1107 <pawan.yadav1107@gmail.com>
2017-09-12 10:14:42 +0530

    change mysqli to pdo

--- OnlineBankingSystem/inc/get_trans_dt.php
+++ OnlineBankingSystem/inc/get_trans_dt.php
@@ -2,8 +2,9 @@
  include 'connection.php';
  include 'getdetails.php';
  // getting all transaction details of the account 
- $trans_result = mysqli_query($conn,"SELECT * FROM transaction WHERE (sender = '".mysqli_real_escape_string($conn,$row_cust["account_no"])."' OR 															receiver = '".mysqli_real_escape_string($conn,$row_cust["account_no"])."')" );
- $row_trans = mysqli_fetch_assoc($trans_result);
+ $trans_result = $conn->prepare($conn,"SELECT * FROM transaction WHERE (sender = '".$row_cust["account_no"]."'  														 										 OR receiver = '".$row_cust["account_no"]."')" );
+ $trans_result->execute();.
+ $row_trans = $trans_result->setFetchMode(PDO::FETCH_ASSOC);
  /*
   suppose we want to show amount tranfered in transaction we can show it by echo $row_trans["money"] and like this every column 
  */
--- OnlineBankingSystem/inc/getdetails.php
+++ OnlineBankingSystem/inc/getdetails.php
@@ -2,14 +2,16 @@
  include 'connection.php';
  //necesarry include to get username
  // getting customer details  
- $cust_result = mysqli_query($conn,"SELECT * FROM customer WHERE username = '".mysqli_real_escape_string($conn,$username). "'");
- $row_cust = mysqli_fetch_assoc($cust_result);
+ $cust_result = $conn->prepare("SELECT * FROM customer WHERE username = '".$username. "'");
+ $cust_result->execute();
+ $row_cust = $cust_result->setFetchMode(PDO::FETCH_ASSOC);
  /*
  we can ascess name by $row_cust["name"] and like this every column 
  */
  //getting account details 
- $acc_result = mysqli_query($conn,"SELECT * FROM account WHERE customer_id = '".mysqli_real_escape_string($conn,$row_cust["customer_id"]). "'");
- $row_acc = mysqli_fetch_assoc($acc_result);
+ $acc_result = $conn->prepare($conn,"SELECT * FROM account WHERE customer_id = '".$row_cust["customer_id"]. "'");
+ $acc_result->execute();
+ $row_acc =$acc_result->setFetchMode(PDO::FETCH_ASSOC);
  /*
  we can ascess balance by $row_acc["balance"] and like this every column 
  */
